1819c9443e9942058f18ecc7c96cce5e63611377
ToiLaHiep02 <92878765+ToiLaHiep02@users.noreply.github.com>
2021-10-21 14:18:15 +0700

    upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Mẹ';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc mẹ iu 20-10';
+const titleCard = 'Tặng Mẹ';
+const contentCard = 'Chúc mẹ của con 20-10 luôn trẻ đẹp và luôn vui vẻ hạnh phúc bên người thương :v';
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
